111cb4740df46665ac52467e85c227c53970f18a
Paulo Montefusco <paulomontefusco.junior@gmail.com>
2025-03-06 13:50:49 -0300

    Criando a rota de Lista de Usuarios

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,11 +2,13 @@ import { Route } from "react-router";
 import { HomePage } from "./pages/Home";
 import { Routes } from "react-router-dom";
 import { Register } from "./pages/Cadastro";
+import { UsersList } from "./pages/Lista-de-usuarios";
 export function Router() {
     return(
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/cadastro" element={<Register />} />
+            <Route path="/lista-de-usuarios" element={<UsersList />} />
         </Routes>
     );
 }
