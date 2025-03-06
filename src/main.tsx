b2963f9ce2fef272edadf590136906123770a198
Paulo Montefusco <paulomontefusco.junior@gmail.com>
2025-03-06 18:30:13 -0300

    Importando no projeto o Toaster

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import './global.css'
 import App from './App.tsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+  <Toaster
+    position="top-center"
+    reverseOrder={true}
+    containerStyle={{
+      fontSize: 16,
+    }}
+      toastOptions={{
+      duration: 2500,
+    }}
+  />
     <App />
   </StrictMode>
 )
