9e4756468e9b5562be3949a03f14cc1808329928
Paulo Montefusco <paulomontefusco.junior@gmail.com>
2025-03-06 14:51:49 -0300

    Criando componente de ListUser e fazendo sua integração

---
+++ src/components/ListUser/index.tsx
@@ -0,0 +1,22 @@
+import styles from "./styles.module.scss";
+import { UserProsp } from "../../@types/interfaces";
+import { publishedDateFormatted } from "../../hooks/formatedDate";
+
+export function ListUser({ data }: UserProsp) {
+    return(
+        <div className={styles.users}>
+            {data && data.map((user) => (
+                <div className={styles.user}>
+                    <div className={styles.informationsUser} key={user.id.value}>
+                        <img src={user.picture.medium} alt="" />
+                        <p><strong>Nome: </strong>{user.name.first}</p>
+                        <p><strong>E-mail: </strong>{user.email}</p>
+                        <p><strong>Nome de Usúario: </strong>{user.login.username}</p>
+                        <p><strong>Data de Nascimento: </strong>{publishedDateFormatted(user.dob.date)}</p>
+                    </div>
+                    <button>Editar</button>
+                </div>
+            ))}
+        </div>
+    );
+}
