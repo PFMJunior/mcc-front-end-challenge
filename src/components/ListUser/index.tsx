505c7593379559ea0a7cc54d0e21ae0845907236
Paulo Montefusco <paulomontefusco.junior@gmail.com>
2025-03-07 10:47:45 -0300

    Alterando o lugar da Key

--- src/components/ListUser/index.tsx
+++ src/components/ListUser/index.tsx
@@ -12,8 +12,8 @@ export function ListUser({ listUsers }: UserProsp) {
         <>
             <div className={styles.users}>
                 {listUsers && listUsers.map((user) => (
-                    <div className={styles.user}>
-                        <div className={styles.informationsUser} key={user.id.value}>
+                    <div className={styles.user} key={user.id.value}>
+                        <div className={styles.informationsUser}>
                             <img src={user.picture.medium} alt="" />
                             <p><strong>Nome: </strong>{user.name.first}</p>
                             <p><strong>E-mail: </strong>{user.email}</p>
