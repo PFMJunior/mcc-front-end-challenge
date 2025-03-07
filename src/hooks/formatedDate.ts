775cce374e1033d2fb4ac75038382f3cc91f99c2
Paulo Montefusco <paulomontefusco.junior@gmail.com>
2025-03-07 10:07:58 -0300

    Altrerando a formatação da Date

--- src/hooks/formatedDate.ts
+++ src/hooks/formatedDate.ts
@@ -3,7 +3,7 @@ import { ptBR } from "date-fns/locale/pt-BR";
 
 const publishedDateFormatted = (date: Date) =>
 {
-    return format(date, "MM/dd/yyyy", {
+    return format(date, "dd/MM/yyyy", {
         locale  : ptBR,
     })
 }
