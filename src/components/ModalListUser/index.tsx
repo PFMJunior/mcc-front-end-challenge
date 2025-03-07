ab91b1085418c7fdc5ca17a94dfd098f83c744c4
Paulo Montefusco <paulomontefusco.junior@gmail.com>
2025-03-07 10:07:31 -0300

    Fazendo a tipagem

--- src/components/ModalListUser/index.tsx
+++ src/components/ModalListUser/index.tsx
@@ -1,9 +1,15 @@
 import styles from "./styles.module.scss";
-import { UserProsp } from "../../@types/interfaces";
+import { ItemModal } from "../../@types/interfaces";
 import { AiOutlineCloseCircle } from "react-icons/ai";
+import { publishedDateFormatted } from "../../hooks/formatedDate";
 
-export function ModalListUser({ close, item }: UserProsp) {
-    console.log(item)
+interface ModalProps {
+    close: (value: boolean) => void;
+    item : ItemModal
+}
+
+export function ModalListUser({ close, item }: ModalProps) {
+    // console.log(item)
     return(
         <div className={styles.modalListUser} onClick={() => close(false)}>
             <div className={styles.container} onClick={(e) => e.stopPropagation()}>
@@ -17,7 +23,15 @@ export function ModalListUser({ close, item }: UserProsp) {
                     <img src={item.picture.large} alt="" />
 
                     <div className={styles.informationsUser}>
-                        
+                        <p><strong>Nome: </strong>{item.name.first}</p>
+                        <p><strong>Sobrenome: </strong>{item.name.last}</p>
+                        <p><strong>E-mail: </strong>{item.email}</p>
+                        <p><strong>Idade: </strong>{item.dob.age} anos</p>
+                        <p><strong>Data de Nascimento: </strong>{publishedDateFormatted(item.dob.date)}</p>
+                        <p><strong>Rua</strong> {item.location.street.name} <strong>Número</strong> {item.location.street.number}</p>
+                        <p><strong>Cidade</strong> {item.location.city}</p>
+                        <p><strong>Estado</strong> {item.location.state}</p>
+                        <p><strong>País</strong> {item.location.country}</p>
                     </div>
                 </div>
             </div>
